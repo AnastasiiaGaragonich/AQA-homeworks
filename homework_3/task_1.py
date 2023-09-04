a406dd6ec03f7cb7703f55cfc0eb757c1808a2b5
AnastasiiaGaragonich <garagonich.nastya@gmail.com>
2023-09-04 21:10:32 +0200

    homework_4

---
+++ homework_3/task_1.py
@@ -0,0 +1,7 @@
+# palindrome
+word = input(("Пожалуйста введите слово: "))
+if (word==word[::-1]):
+    print("+")
+else:
+    print("-")
+
---
+++ homework_3/task_4.py
@@ -0,0 +1,5 @@
+user = input("Enter text: ")
+var = user.split()
+print(var)
+print(user[-3:])
+
