83ce196147911aafa539b8ab23aff4d8279bfe1a
AnastasiiaGaragonich <garagonich.nastya@gmail.com>
2023-09-04 15:30:56 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# git init
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/AnastasiiaGaragonich/AQA-homeworks.git
+# git push -u origin main
+# git clone <url>
